c8a0bf08b4cd55894b24953c2ad2ddf7c3c06250
Ramom <ramoncezar531@gmail.com>
2023-09-21 12:37:12 -0300

    Create UserService

---
+++ src/main/java/com/example/ramomlopes/todosimple/services/UserService.java
@@ -0,0 +1,42 @@
+package com.example.ramomlopes.todosimple.services;
+
+import com.example.ramomlopes.todosimple.models.User;
+import com.example.ramomlopes.todosimple.repositories.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User findById(Long id){
+        return this.userRepository.findById(id).get();
+    }
+
+    @Transactional
+    public User create(User obj){
+        obj.setId(null);
+        obj = this.userRepository.save(obj);
+        return obj;
+    }
+
+    @Transactional
+    public User update(User obj){
+        User newObj = findById(obj.getId());
+        newObj.setPassword(obj.getPassword());
+        return this.userRepository.save(newObj);
+    }
+
+    public User delete(Long id){
+        findById(id);
+        try {
+            this.userRepository.deleteById(id);
+        }catch (Exception e){
+            throw new RuntimeException("Não é possível excluir");
+        }
+    }
+
+}
